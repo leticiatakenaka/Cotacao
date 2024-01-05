c81243bea565dc095b2d3b9a4f0350e18b859fdd
Leticia Santos Takenaka <leticiatakenaka12@gmail.com>
2024-01-05 08:18:51 -0300

    Adicionar arquivos de projeto.

---
+++ Cotacao/ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+﻿namespace Cotacao
+{
+    class ConversorDeMoeda
+    {
+        public static double _cotacaoDolar;
+
+        public static void LerCotacao(double cotacaoDolar)
+        {
+            _cotacaoDolar = cotacaoDolar;
+        }
+
+        public static double ValorASerPago(double qtd)
+        {
+            double total = qtd * _cotacaoDolar;
+            return total + total / 100 * 6;
+        }
+    }
+}
---
+++ Cotacao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Cotacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            ConversorDeMoeda.LerCotacao(cotacao);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"O valor a ser pago é {ConversorDeMoeda.ValorASerPago(quantia)}");
+        }
+    }
+}
